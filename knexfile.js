9c9af93631ca5ae1a8a9a1af149d19ac91730e02
TI Akta <rodrigomendoncca@gmail.com>
2020-04-01 16:16:58 -0300

    Create database

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,15 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: 'pg',
+    connection:'process.env.DATABASE_URL',
+    migrations: {
+      directory: 'src/database/migrations'
+    },
+    seeds: {
+      directory: 'src/database/seeds'
+    },
+    useNullAsDefault: true
   },
 
   staging: {
@@ -26,19 +31,14 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection:'process.env.DATABASE_URL',
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: 'src/database/migrations'
+    },
+    seeds: {
+      directory: 'src/database/seeds'
+    },
+    useNullAsDefault: true
   }
-
 };
--- src/controllers/InfoController.js
+++ src/controllers/InfoController.js
@@ -1,7 +1,11 @@
 module.exports ={
     index(req,res){
         return res.json({
-            'Name': 'Rodrigo Cordeiro',
+            Name: 'Rodrigo Cordeiro',
+            email:'rodrigomendoncca@gmail.com',
+            phone:'11 9 69187148',
+            whatsapp: '+5511969187148',
+            telegram: 'rodcodeiro'
         });
     },
 }
