b36c37d62af33043a84de8edac649930b53586c9
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-04-04 21:23:23 -0300

    Update files

--- knexfile.js
+++ knexfile.js
@@ -42,7 +42,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'postgres://ybjcwusmyqtdcw:ae58fd014a82f7f6a04699c0f1f9a5a76d1bf961d2cad33be21a4a9738f106fb@ec2-52-6-143-153.compute-1.amazonaws.com:5432/dao504m8mr8965',
+    connection: 'postgres://rpmqgarivkcudr:31e29bf3f3e681005f292d20b9b38dcebedf362470082bc2023dd9e23b056894@ec2-34-200-116-132.compute-1.amazonaws.com:5432/dfvi0hjacijsbd',
     migrations: {
       directory: './src/database/migrations'
     },
