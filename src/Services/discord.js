6038284bd6f4942b0c5dc9cdef661a8e2c80b513
TI Akta <rodrigomendoncca@gmail.com>
2020-04-14 17:39:15 -0300

    update Services/discord

--- src/Services/discord.js
+++ src/Services/discord.js
@@ -1,22 +1,26 @@
-const api = require ('./Services/axios');
+const api = require ('./axios');
 const discordURL = 'https://discordapp.com/api/'
 
 module.exports ={
-    sendMessage(req,res){
+    async sendMessage(req,res){
         const webhook = 'webhooks/699699746855452673/DQMga4GEYErNQ2dYCEciHVhy8RjF7Ce3ODPRnWiBDO-HaNqlIiWw2asrePG62Qw-YXcw'
+        console.log(req.body)
         try {
             await api.post(discordURL + webhook, {
-                "content":"msg",
+                "content":"req.body",
                 "username":"Lord Darth Vader",
                 "avatar_url": "https://rodcordeiro.github.io/shares/img/vader.png"
             })
               .then(function (response) {
-                console.log(response);
+                // console.log(response);
               })
               .catch(function (error) {
-                console.log(error);
+                // console.log(error);
               });
-        
+            return res.status(200).json({message:"Message sent"})
+        } catch(err){
+            console.log(err)
+            return res.status(400).json({message:"BadRequest"})
         }
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const routes = express.Router();
 const projectController = require('./controllers/projectControllers');
 
 const api = require ('./Services/axios');
-
+const discord = require('./Services/discord');
 routes.get('/', function (req, res){
     return res.status(200).json({
         "Name": "CordeiroAPI",
@@ -15,23 +15,7 @@ routes.get('/', function (req, res){
 routes.get('/projects', projectController.index);
 
 
-routes.post('/habitica', (req, res) => {
-    console.log(req.body);
-    return res.status(200).json({
-        message:"Webhook received"
-    });
-})
+routes.post('/habitica', discord.sendMessage)
 
-routes.post('/discord', async (req,res)=>{
-    const msg = `
-**New Task:**
-    ${req.body.text}
-`   
-    try{
-         return res.status(200).json({message: "Message sent" })
-
-    } catch(error){
-        return res.status(400).json({message: "Houston we got a problem"})
-    }    
-})
+routes.post('/discord', discord.sendMessage)
 module.exports = routes;
