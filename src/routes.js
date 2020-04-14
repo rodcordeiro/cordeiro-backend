dcc592262063b0fb2f3fd8f8b346c2556213c04c
TI Akta <rodrigomendoncca@gmail.com>
2020-04-14 15:37:00 -0300

    Created routes

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,19 @@ const routes = express.Router();
 const projectController = require('./controllers/projectControllers');
 
 routes.get('/', function (req, res){
-    return res.send('ok');
+    return res.status(200).json({
+        "Name": "CordeiroAPI",
+        "Version": "1.0"
+      });
 })
 
 routes.get('/projects', projectController.index);
 
+
+routes.post('/habitica', (req, res) => {
+    console.log(req.body);
+    return res.status(200).json({
+        message:"Webhook received"
+    });
+})
 module.exports = routes;
