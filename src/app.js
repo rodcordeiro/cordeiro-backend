9b19f7b9494bdfe5ca9801ad387031fa82497fc1
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-09-27 17:10:20 -0300

    Edit cors configuration

--- src/app.js
+++ src/app.js
@@ -2,13 +2,20 @@ const express = require("express");
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+const corsOptions = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }
+
 dotenv.config();
 
 const app = express();
 const routes = require('./routes');
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(routes);
 
 module.exports = app;
