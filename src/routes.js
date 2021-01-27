56f1cbbd0e3d077caabe93ec703bbeee3a4d83ab
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-01-27 16:49:33 -0300

    teste

--- src/routes.js
+++ src/routes.js
@@ -63,7 +63,7 @@ routes.post('/devpost', devtoController.createPost)
 
 routes.post('/teste', (req,res)=>{
   const body = req.body
-  console.log(body)
+  console.log({body})
   return res.status(200).json({body})
 })
 
