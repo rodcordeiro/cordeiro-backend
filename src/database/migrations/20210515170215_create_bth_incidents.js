4a32553311d9e380299fa3528c76f35c2ea79025
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-05-15 18:43:11 -0300

    Modified incidents table

--- src/database/migrations/20210515170215_create_bth_incidents.js
+++ src/database/migrations/20210515170215_create_bth_incidents.js
@@ -6,7 +6,7 @@ exports.up = function(knex) {
       table.string('description').notNullable();
       table.decimal('value').notNullable();
       table.string('ong_id').notNullable();
-      table.foreign('ong_id').references('id').inTable('ongs');
+      table.foreign('ong_id').references('id').inTable('bth_ongs');
     });
   };
   
