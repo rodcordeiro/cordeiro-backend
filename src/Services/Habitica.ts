f7df85d0de065007e487f469923b8c4d6b825eb1
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-08-15 20:57:50 -0300

    Add webhook management functionallity

--- src/Services/Habitica.ts
+++ src/Services/Habitica.ts
@@ -4,36 +4,56 @@ import { habiAPI } from '../tools/api'
 class HabiticaService{
     async webhookHandler(data: any){
         return new Promise(async(resolve,reject)=>{
-            if(data.task.id != "b322a291-87c4-490e-8bf6-2b7087538929"){
-                const task = await habiAPI.get('/tasks/b322a291-87c4-490e-8bf6-2b7087538929',{
-                    headers: {
-                        'x-api-user': 'c150cf43-bf4a-4c46-8912-9c04f77d3924',
-                        'x-api-key': '3a00e702-525c-41f2-a69a-d10b741b0c5c',
-                        'x-client': 'c150cf43-bf4a-4c46-8912-9c04f77d3924-cordeiroAPI'
-                      }
-                }).then(response=>{
-                    return response.data.data
-                })
-                const checklistItems = task.checklist.length -1
-                checklistItems.map(async (item : any,index: number)=>{
-                    if(!item.completed){
-                        await habiAPI.post(`/tasks/b322a291-87c4-490e-8bf6-2b7087538929/checklist/${item.id}/score`,{
-                            headers: {
-                                'x-api-user': 'c150cf43-bf4a-4c46-8912-9c04f77d3924',
-                                'x-api-key': '3a00e702-525c-41f2-a69a-d10b741b0c5c',
-                                'x-client': 'c150cf43-bf4a-4c46-8912-9c04f77d3924-cordeiroAPI'
-                              }
-                        })
-                    }
-                    if(index == checklistItems)
-                })
+            if(data.type == "scored" || data.type == "updated" || data.type == "checklistScored"){
+                if(data.task.id != "b322a291-87c4-490e-8bf6-2b7087538929"){
+                    const task = await habiAPI.get('/tasks/b322a291-87c4-490e-8bf6-2b7087538929',{
+                        headers: {
+                            'x-api-user': 'c150cf43-bf4a-4c46-8912-9c04f77d3924',
+                            'x-api-key': '3a00e702-525c-41f2-a69a-d10b741b0c5c',
+                            'x-client': 'c150cf43-bf4a-4c46-8912-9c04f77d3924-cordeiroAPI'
+                          }
+                    }).then(response=>{
+                        return response.data.data
+                    })
+                    const checklistItems = task.checklist.length
+                    let completed : number = 0;
+                    let check : boolean = false 
 
+                    task.checklist.map(async (item : any,index: number)=>{
+                        if(item.completed){
+                            completed++;
+                        } else {
+                            await habiAPI.post(`/tasks/b322a291-87c4-490e-8bf6-2b7087538929/checklist/${task.checklist[completed].id}/score`,{},{
+                                headers: {
+                                    'x-api-user': 'c150cf43-bf4a-4c46-8912-9c04f77d3924',
+                                    'x-api-key': '3a00e702-525c-41f2-a69a-d10b741b0c5c',
+                                    'x-client': 'c150cf43-bf4a-4c46-8912-9c04f77d3924-cordeiroAPI'
+                                  }
+                            }).catch(err=>{
+                                console.log({err})
+                            })
+                            if(completed == checklistItems -1){
+                                await habiAPI.post("/tasks/b322a291-87c4-490e-8bf6-2b7087538929/score/up",{},{
+                                    headers: {
+                                        'x-api-user': 'c150cf43-bf4a-4c46-8912-9c04f77d3924',
+                                        'x-api-key': '3a00e702-525c-41f2-a69a-d10b741b0c5c',
+                                        'x-client': 'c150cf43-bf4a-4c46-8912-9c04f77d3924-cordeiroAPI'
+                                      }
+                                    }).catch(err=>{
+                                        console.log({err})
+                                    })
+                            }
+                        }
+                    }) 
+                }
             }
 
 
             const discord = new DiscordService();
             await discord.send_message('habitica_news',`**Task ${data.type}**
-            task: ${data}`,{
+            task: ${data.task.text}
+            description: ${data.task.notes}\n`,{
+                username: "Grifo",
     avatar_url: "https://habitica.com/static/img/melior@3x.fe3b187f.png"
 })
                 .then(response=>{
@@ -49,9 +69,6 @@ export {
     HabiticaService
 };
 
-// `**Task ${data.type}**
-// task: ${data.task.text}
-// description: ${data.task.notes}\n`
 // tasks id b322a291-87c4-490e-8bf6-2b7087538929
 // POST https://habitica.com/api/v3/tasks/:taskId/checklist/:itemId/score
 // "checklist": [
