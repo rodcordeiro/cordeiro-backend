1092aef35e49c192ea5d0639bc7e3619001ddf9e
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-08-15 23:57:15 -0300

    Add build workflow

--- build/src/components/Fin/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-const express_1 = require("express");
-const routes = express_1.Router();
-exports.routes = routes;
-routes.get('/', (req, res) => {
-    return res.status(200).send("ok");
-});
