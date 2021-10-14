50fea716acfcbd81c01fd23354191381a00dc7ef
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2021-10-14 18:43:32 -0300

    add prettier

--- src/Controllers/Github.ts
+++ src/Controllers/Github.ts
@@ -1,59 +1,59 @@
-import {Request, Response} from "express";
-import { GHAuth, GHapi } from "../tools/api";
+import { Request, Response } from 'express';
+import { GHAuth, GHapi } from '../tools/api';
 
-
+/* resk start "GithubAuthentication" */
 class GithubAuth {
-    async get_token(req : Request, res: Response){
-        const redirect_uri = "http://rodcordeiro.herokuapp.com/gh/token"
-        const origin = decodeURIComponent(String(req.query.origin));
-        const state = Buffer.from(origin).toString('base64');
-        const url =  `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUBAPP_CLIENT_ID}&redirect_uri=${encodeURIComponent(redirect_uri)}&scope=repo%20user%20delete_repo&state=${state}&allow_signup=false`;
-        
-        res.redirect(url)
-    }
-    async validateToken(req : Request,res: Response){
-        const code : any = req.query.code;
-        const state : any  = req.query.state;
-        const origin : any = Buffer.from(state,"base64").toString('ascii');
-        const redirect_uri = "http://rodcordeiro.herokuapp.com/gh/token"
+  async get_token(req: Request, res: Response) {
+    const redirect_uri = 'http://rodcordeiro.herokuapp.com/gh/token';
+    const origin = decodeURIComponent(String(req.query.origin));
+    const state = Buffer.from(origin).toString('base64');
+    const url = `https://github.com/login/oauth/authorize?client_id=${
+      process.env.GITHUBAPP_CLIENT_ID
+    }&redirect_uri=${encodeURIComponent(
+      redirect_uri,
+    )}&scope=repo%20user%20delete_repo&state=${state}&allow_signup=false`;
 
-        await GHAuth.post("/access_token",{
-            "client_id": process.env.GITHUBAPP_CLIENT_ID,
-            "client_secret": process.env.GITHUBAPP_CLIENT_SECRET,
-            "code": code,
-            "state":state,
-            "redirect_uri":redirect_uri
-          })
-          .then((response: any)=>{
-            const token = response.data.split("&")[0].split("=")[1]  
-            res.redirect(`${origin}?token=${token}`)
-            
-          })
-          .catch((err: any)=>{
-            console.log({err})
-            return res.status(400).json(err)
-        })
-    } 
-}
+    res.redirect(url);
+  }
+  async validateToken(req: Request, res: Response) {
+    const code: any = req.query.code;
+    const state: any = req.query.state;
+    const origin: any = Buffer.from(state, 'base64').toString('ascii');
+    const redirect_uri = 'http://rodcordeiro.herokuapp.com/gh/token';
 
-class GithubController{
-    async getRepos(req : Request, res: Response){
-        const {token} = req.body
-        await GHapi.get(`/user/repos`,{
-            headers:{
-                "authorization": token
-              }
-        })
-        .then((response) => {
-            return res.status(200).json(response.data)            
-        })
-        .catch(err=>{
-            return res.status(400).json(err)
-        })
+    await GHAuth.post('/access_token', {
+      client_id: process.env.GITHUBAPP_CLIENT_ID,
+      client_secret: process.env.GITHUBAPP_CLIENT_SECRET,
+      code: code,
+      state: state,
+      redirect_uri: redirect_uri,
+    })
+      .then((response: any) => {
+        const token = response.data.split('&')[0].split('=')[1];
+        res.redirect(`${origin}?token=${token}`);
+      })
+      .catch((err: any) => {
+        console.log({ err });
+        return res.status(400).json(err);
+      });
+  }
+}
+/* resk end "GithubAuthentication" */
 
-    }
+class GithubController {
+  async getRepos(req: Request, res: Response) {
+    const { token } = req.body;
+    await GHapi.get(`/user/repos`, {
+      headers: {
+        authorization: token,
+      },
+    })
+      .then((response) => {
+        return res.status(200).json(response.data);
+      })
+      .catch((err) => {
+        return res.status(400).json(err);
+      });
+  }
 }
-export {
-    GithubAuth,
-    GithubController
-}
+export { GithubAuth, GithubController };
